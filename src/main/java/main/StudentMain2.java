773ce94f937ae4e9784c7d9c5347754e6b5a85d9
KimJunsu <sbn04043@gmail.com>
2024-05-31 19:32:21 +0900

    commit check

--- src/main/java/main/StudentMain2.java
+++ src/main/java/main/StudentMain2.java
@@ -17,55 +17,5 @@ public class StudentMain2 {
         studentViewer.setScanner(scanner);
 
         studentViewer.showList();
-
-
-//        System.out.println("===================================");
-//        ArrayList<StudentDTO> studentList = studentController.selectAll();
-//        for (StudentDTO s : studentList) {
-//            System.out.println(s);
-//        }
-//
-//        System.out.println("===================================");
-//        StudentDTO student = studentController.selectOne(3);
-//        System.out.println(student);
-//
-//        System.out.println("===================================");
-//        StudentDTO student2 = new StudentDTO();
-//
-//        student2.setName("바바바");
-//        student2.setKorean(50);
-//        student2.setEnglish(50);
-//        student2.setMath(50);
-//
-//        //studentController.insert(student2);
-//
-////        studentList = studentController.selectAll();
-////        for (StudentDTO s : studentList) {
-////            System.out.println(s);
-////        }
-//
-//        System.out.println("===================================");
-//        StudentDTO student3 = new StudentDTO();
-//        student3.setId(3);
-//        student3.setName("사사사");
-//        student3.setKorean(100);
-//        student3.setEnglish(100);
-//        student3.setMath(100);
-//
-//        //studentController.update(student3);
-//
-////        studentList = studentController.selectAll();
-////        for (StudentDTO s : studentList) {
-////            System.out.println(s);
-////        }
-//
-//        System.out.println("===================================");
-//        studentController.delete(6);
-//        studentList = studentController.selectAll();
-//        for (StudentDTO s : studentList) {
-//            System.out.println(s);
-//        }
-//
-//        System.out.println("===================================");
     }
 }
